fc42037eb387277ed5919984f3d7ab77c4269d62
Binhua Da <binhuada@Binhuas-MacBook-Pro.local>
2025-08-28 12:30:33 +1000

    formgroup layout

--- src/pages/Settings/AlertsSettings/index.tsx
+++ src/pages/Settings/AlertsSettings/index.tsx
@@ -11,9 +11,18 @@ import {
   Container,
   Paper,
   Divider,
+  Typography,
+  Button,
 } from '@mui/material';
 
-const PPE = ['Helmet', 'Mask', 'Reflective Vest', 'Safety Gloves', 'Goggles', 'Safety Boots'];
+const PPET: string[] = [
+  'Helmet',
+  'Mask',
+  'Reflective Vest',
+  'Safety Gloves',
+  'Goggles',
+  'Safety Boots',
+];
 
 export default function AlertSettings(): React.ReactElement {
   return (
@@ -23,9 +32,39 @@ export default function AlertSettings(): React.ReactElement {
         <Divider />
         <CardContent>
           <Grid container sx={{ flex: 1 }} direction="column">
-            <Grid size={{ xs: 12 }}>ppe</Grid>
-            <Grid size={{ xs: 12 }}>notification</Grid>
-            <Grid size={{ xs: 12 }}>save</Grid>
+            <Grid size={{ xs: 12 }}>
+              <Typography> PPE Type</Typography>
+              <FormGroup
+                row
+                sx={{
+                  flexWrap: 'wrap',
+                  justifyContent: 'center',
+                  '& .MuiFormControlLabel-root': {
+                    width: { xs: '100%', md: '45%' },
+                    mx: 1,
+                  },
+                }}
+              ></FormGroup>
+            </Grid>
+            <Grid size={{ xs: 12 }}>
+              <Typography> Notifications Type</Typography>
+              <Grid container spacing={1}>
+                <FormGroup>
+                  <Grid container spacing={2} justifyContent={'center'} alignItems={'center'}>
+                    {['EMAIL', 'SMS'].map((p, index) => (
+                      <Grid key={p} size={{ xs: 12, md: 6 }}>
+                        <FormControlLabel label={p} control={<Checkbox />} />
+                      </Grid>
+                    ))}
+                  </Grid>
+                </FormGroup>
+              </Grid>
+            </Grid>
+            <Grid size={{ xs: 12 }}>
+              <Grid container justifyContent={'center'} sx={{ mt: 4 }}>
+                <Button>SAVE</Button>
+              </Grid>
+            </Grid>
           </Grid>
         </CardContent>
       </Card>
